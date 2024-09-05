b8dba45d0926386c2193af5eddddc45e0a7b51a2
THUNDERluzer <95018914+THUNDERluzer@users.noreply.github.com>
2024-09-06 04:29:14 +0530

    Update settings.js

--- plugins/settings.js
+++ plugins/settings.js
@@ -10,7 +10,7 @@ cmd({
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
         return await conn.sendMessage(from, {
-            image: { url: 'https://telegra.ph/file/959a00b8f92106a8856de.jpg' },
+            image: { url: 'https://telegra.ph/file/c8b2e77d86e36ca9414e8.jpg' },
             caption: '𝗗𝗜𝗟𝗔𝗠𝗗 𝗦𝗘𝗧𝗧𝗜𝗡𝗚𝗦 ⚙️\n\n*Cheng Alive&Menu IMG*\n.update ALIVE_IMG: Your Image Url\n\n*Cheng Alive MSG*\n.update ALIVE_MSG: Your Alive MSG\n\n*Cheng Prefix*\n.update PERFIX: your prefix (.,$#%&)\n\n*Auto Status Seen*\n.update AUTO_READ_STATUS: true or false\n\n*Mode*\n.update MODE: private, public, index or group'
         }, { quoted: mek });
     } catch (e) {
