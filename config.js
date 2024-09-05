0ca476bf5baddb604cce8a454b3de6dd64b513f7
vimanew <kethmi12345@gmail.com>
2024-09-06 00:08:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,5 +7,5 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "G2QnxCgK#T5_HoMK5BFw5AMPam402CgzeCHxhkh73x2XvFDuW31U",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "public",
+MODE: process.env.MODE || "private",
 };
