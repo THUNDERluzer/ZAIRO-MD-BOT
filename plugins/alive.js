7e597dbaeda8d64e3d1c2e800c961ae44778d8e3
vimanew <kethmi12345@gmail.com>
2024-09-06 00:45:58 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,25 +1,29 @@
-const config = require('../config') 
-const {cmd, commands} = require('../command') 
-const os = require("os") 
+const config = require('../config')
+const {cmd, commands} = require('../command')
+const os = require("os")
 const {runtime} = require('../lib/functions')
 
 cmd({
- pattern: "alive", 
-react: "📰",
-desc: "Check uptime, RAM usage, and more", category: "main", filename: __filename }, 
+    pattern: "alive",
+    react: "📰",
+    desc: "Check uptime, RAM usage, and more",
+    category: "main",
+    filename: __filename
+}, async (conn, mek, m, {from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+    try {
 
-
-async (conn, mek, m, {from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => { try 
-
-{ // RAM usage const totalRAM = Math.round(require('os').totalmem() / 1024 / 1024); 
-
-// Total RAM in MB const usedRAM = (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2); // Used RAM in MB const freeRAM = (totalRAM - parseFloat(usedRAM)).toFixed(2); // Free RAM in MB
+// RAM usage
+        const totalRAM = Math.round(require('os').totalmem() / 1024 / 1024); // Total RAM in MB
+        const usedRAM = (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2); // Used RAM in MB
+        const freeRAM = (totalRAM - parseFloat(usedRAM)).toFixed(2); // Free RAM in MB
 
 
 let status = `*✸𝕎𝔼𝕃ℂ𝕆𝕄𝔼 𝕋𝕆 ℤ𝔸𝕀ℝ𝕆 𝕄𝔻 𝔹𝕆𝕋✸*
 
 
-𝙽𝙾𝚆 𝚄𝚂𝙴𝙳 𝚁𝙰𝙼: ${usedRAM} 𝙼𝙱 𝚄𝙿 𝚃𝙸𝙼𝙴: ${runtime(process.uptime())} ®𝚅𝙸𝙼𝙰𝙼𝙾𝙳𝚂®`
+𝙽𝙾𝚆 𝚄𝚂𝙴𝙳 𝚁𝙰𝙼: ${usedRAM} 𝙼𝙱 
+𝚄𝙿 𝚃𝙸𝙼𝙴: ${runtime(process.uptime())} 
+®𝚅𝙸𝙼𝙰𝙼𝙾𝙳𝚂®`
 
 // URL of the image you want to include
     const imageUrl = 'https://ibb.co/L86DZLX'; 
@@ -30,4 +34,10 @@ let status = `*✸𝕎𝔼𝕃ℂ𝕆𝕄𝔼 𝕋𝕆 ℤ𝔸𝕀ℝ𝕆 𝕄�
     await conn.sendMessage(from, {
         image: { url: imageUrl },
         caption: status
-    }, { quoted: mek || null });} catch (e) { console.log(e) reply(Error: ${e}) } })
+    }, { quoted: mek || null });
+
+ } catch (e) {
+        console.log(e)
+        reply(`Error: ${e}`)
+    }
+})
